56fabddae948feeee9f1af0233b4d78cd52282ba
amir-100 <66202558+amir-100@users.noreply.github.com>
2026-01-12 22:12:30 +0200

    Make onBallClick increase ball size

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 "use strict";
 
-const onBallClick = () => {
-  console.log("Ball Clicked");
+const onBallClick = (elBall) => {
+  const size = parseFloat(elBall.style.getPropertyValue("--size") || 100);
+  const newSize = size + 50;
+
+  elBall.style.setProperty("--size", `${newSize}px`);
+  elBall.textContent = newSize;
 };
