e0c8d46a918e535a367dd119f61a774dc544d71c
amir-100 <66202558+amir-100@users.noreply.github.com>
2026-01-12 23:31:55 +0200

    Add max diameter to each ball

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const onBallClick = (elBall) => {
+const onBallClick = (elBall, maxDiameter) => {
   const step = getRandomInt(20, 60);
-  const max = 400;
+  const max = maxDiameter;
   const min = 100;
   const size = parseFloat(elBall.style.getPropertyValue("--size") || min);
   const nextSize = size + step;
