7412b9cbdf042c252c42af0549d938a151472407
amir-100 <66202558+amir-100@users.noreply.github.com>
2026-01-13 11:24:51 +0200

    Swap first two balls' size and color on click 3rd ball

--- main.js
+++ main.js
@@ -14,3 +14,26 @@ const onBallClick = (elBall, maxDiameter) => {
   elBall.textContent = newSize;
   elBall.style.backgroundColor = getRandomColor();
 };
+
+const onThirdBallClick = () => {
+  const elBall1 = document.querySelector(".ball1");
+  const elBall2 = document.querySelector(".ball2");
+
+  const style1 = getComputedStyle(elBall1);
+  const style2 = getComputedStyle(elBall2);
+
+  const { backgroundColor: color1 } = style1;
+  const { backgroundColor: color2 } = style2;
+
+  const size1 = parseInt(style1.getPropertyValue("--size"));
+  const size2 = parseInt(style2.getPropertyValue("--size"));
+
+  elBall1.style.setProperty("--size", `${size2}px`);
+  elBall2.style.setProperty("--size", `${size1}px`);
+
+  elBall1.textContent = size2;
+  elBall2.textContent = size1;
+
+  elBall1.style.backgroundColor = color2;
+  elBall2.style.backgroundColor = color1;
+};
